cf0c23c3b2cb52b90ddbbac47d24b917ca156dbc
Nicoam89 <nico.am98@gmail.com>
2025-08-25 14:30:54 -0300

    se modifica main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <>
     <App />
-  </React.StrictMode>
+  </>,
 )
