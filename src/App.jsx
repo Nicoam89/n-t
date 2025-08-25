c53665d6fb1601a35ab89c48f014d8048b494801
Nicoam89 <nico.am98@gmail.com>
2025-08-25 14:17:52 -0300

    se corrijen imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
-import './components/Header';
-import './components/Body';
-import './components/Footer';
+import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Header/>
-      <Body/>
-      <Footer/>
+      <Header />
+      <Body />
+      <Footer />
     </>
   )
 }
