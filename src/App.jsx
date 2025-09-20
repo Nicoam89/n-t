011d35d6ce7d0e3478ed6b1e76321c342911eff2
Nicoam89 <nico.am98@gmail.com>
2025-09-20 20:02:12 -0300

    se corrige error header

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ItemListContainer from "./containers/ItemListContainer";
 import ItemDetailContainer from "./containers/ItemDetailContainer";
+import Header from "./components/Header";
 
 function App() {
   return (
